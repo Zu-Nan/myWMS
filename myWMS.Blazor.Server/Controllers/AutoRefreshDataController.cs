d2467000923234452a0e8717a2b0857c0c527cc3
Zu-Nan <2309994895@qq.com>
2026-05-29 10:50:40 +0800

    自动刷新

---
+++ myWMS.Blazor.Server/Controllers/AutoRefreshDataController.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor;
+using System.Timers;
+
+namespace wms.Blazor.Server.Controllers
+{
+    public class AutoRefreshDataController:ViewController<ListView>
+    {
+        private System.Timers.Timer _refreshTimer;
+        private const int RefreshIntervalMs = 3000;
+
+        protected override void OnActivated()
+        {
+            //初始化定时器
+            _refreshTimer = new System.Timers.Timer(RefreshIntervalMs);
+            _refreshTimer.Elapsed += OnTImerElapsed;
+            _refreshTimer.AutoReset = true;
+            _refreshTimer.Start();
+        }
+
+        private void OnTImerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if(Application is BlazorApplication blazorApp && View != null && !View.IsDisposed)
+            {
+                blazorApp.InvokeAsync(() =>
+                {
+                    //只有在没有选中对象时才刷新，避免丢失选择
+                    if (View.SelectedObjects.Count == 0)
+                    {
+                        View.RefreshDataSource();
+                    }
+                });
+            }
+        }
+
+        protected override void OnDeactivated()
+        {
+            base.OnDeactivated();
+            //释放定时器
+            _refreshTimer?.Stop();
+            _refreshTimer?.Dispose();
+        }
+    }
+}
