7c8e620e881bb55899b5a7919db37cc745b3ecca
Zu-Nan <2309994895@qq.com>
2026-05-29 11:16:59 +0800

    新增对象入口和出口

---
+++ myWMS.Module/BusinessObjects/JiChuDate/ChuKou.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpo;
+
+namespace myWMS.Module.BusinessObjects.JiChuDate
+{
+    //出口对象
+    public class ChuKou : XPObject
+    {
+        public ChuKou(Session session) : base(session) { }
+
+        private string _chuKouNum;
+        [DisplayName("出口编号")]
+        public string ChuKouNum
+        {
+            get => _chuKouNum;
+            set => SetPropertyValue(nameof(ChuKouNum), ref _chuKouNum, value);
+        }
+
+        private string _chuKouName;
+        [DisplayName("出口名称")]
+        public string ChuKouName
+        {
+            get => _chuKouName;
+            set => SetPropertyValue(nameof(ChuKouName), ref _chuKouName, value);
+        }
+
+        private string _baoHao;
+        [DisplayName("系统包号")]
+        public string BaoHao
+        {
+            get => _baoHao;
+            set => SetPropertyValue(nameof(BaoHao), ref _baoHao, value);
+        }
+
+        private bool _qiYong;
+        [DisplayName("启用")]
+        public bool QiYong
+        {
+            get => _qiYong;
+            set => SetPropertyValue(nameof(QiYong), ref _qiYong, value);
+        }
+
+        private DateTime _time;
+        [DisplayName("更新时间")]
+        public DateTime Time
+        {
+            get => _time;
+            set => SetPropertyValue(nameof(Time), ref _time, value);
+        }
+
+        private string _beiZhu;
+        [DisplayName("备注")]
+        [Size(100)]
+        public string BeiZhu
+        {
+            get => _beiZhu;
+            set => SetPropertyValue(nameof(BeiZhu), ref _beiZhu, value);
+        }
+
+         protected override void OnSaving()
+        {
+            base.OnSaving();
+            Time = DateTime.Now;
+        }
+    }
+}
---
+++ myWMS.Module/BusinessObjects/JiChuDate/RuKou.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpo;
+
+namespace myWMS.Module.BusinessObjects.JiChuDate
+{
+    //入口对象
+    public class RuKou : XPObject
+    {
+        public RuKou(Session session) : base(session) { }
+
+        private string _ruKouNum;
+        [DisplayName("入口编号")]
+        public string RuKouNum
+        {
+            get => _ruKouNum;
+            set => SetPropertyValue(nameof(RuKouNum), ref _ruKouNum, value);
+        }
+
+        private string _ruKouName;
+        [DisplayName("入口名称")]
+        public string RuKouName
+        {
+            get => _ruKouName;
+            set => SetPropertyValue(nameof(RuKouName), ref _ruKouName, value);
+        }
+
+        private string _baoHao;
+        [DisplayName("系统包号")]
+        public string BaoHao
+        {
+            get => _baoHao;
+            set => SetPropertyValue(nameof(BaoHao), ref _baoHao, value);
+        }
+
+        private bool _qiYong;
+        [DisplayName("启用")]
+        public bool QiYong
+        {
+            get => _qiYong;
+            set => SetPropertyValue(nameof(QiYong), ref _qiYong, value);
+        }
+
+        private DateTime _time;
+        [DisplayName("更新时间")]
+        public DateTime Time
+        {
+            get => _time;
+            set => SetPropertyValue(nameof(Time), ref _time, value);
+        }
+
+        protected override void OnSaving()
+        {
+            base.OnSaving();
+            Time = DateTime.Now;
+        }
+    }
+}
